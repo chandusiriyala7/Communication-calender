45f9cf220305ce0765083a419c8c9adc3655ab73
chandusiriyala <chandusiriyala116@gmail.com>
2025-01-02 23:39:00 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "https://communication-calender-iota.vercel.app/" }));
+app.use(cors({ origin: "https://communication-calender-iota.vercel.app" }));
 
 
 
