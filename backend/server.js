db1c0f5e35a35a273b2ee7a74925c5ce77530943
chandusiriyala <chandusiriyala116@gmail.com>
2025-01-02 23:34:43 +0530

    deployment error resolved

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const app = express();
 app.use(cors({ origin: "https://communication-calender-iota.vercel.app/" }));
 
 
-const app = express();
 
 // Middleware
 app.use(cors());
